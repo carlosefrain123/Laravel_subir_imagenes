aecca5fb5bdbf503affb4d6e55f9d6ed85d4bc19
Carlos Efrain Chafloque LLontop <122811704+carlosefrain123@users.noreply.github.com>
2024-05-12 00:17:15 -0500

    12/05/2024 - 00:15 - Se agrega en el proyecto create.blade las librerias de Dropzone y su js - En app.blade se modifica el layout con el yield para agregar el content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,9 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+    <!-- CSS -->
+    @yield('css')
+
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -32,5 +35,8 @@
             @yield('content')
         </main>
     </div>
+
+    <!-- JS -->
+    @yield('js')
 </body>
 </html>
