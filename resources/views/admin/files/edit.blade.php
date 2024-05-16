24e6821d584d83379be666111bf10191d1902edd
Carlos Efrain Chafloque LLontop <122811704+carlosefrain123@users.noreply.github.com>
2024-05-16 11:35:49 -0500

    16/05/2024 - 11:35 - Se cambia las interfaces al estilos de Dropzone

--- resources/views/admin/files/edit.blade.php
+++ resources/views/admin/files/edit.blade.php
@@ -1,23 +1,30 @@
 @extends('layouts.app')
-
+@section('css')
+    <link href="https://cdn.jsdelivr.net/npm/dropzone@5/dist/min/dropzone.min.css" rel="stylesheet">
+@endsection
 @section('content')
     <div class="w-full mx-auto">
         <div class="max-w-md mx-auto bg-white shadow-md rounded p-6">
             <h2 class="text-xl font-semibold mb-4">Editar Imagen</h2>
-            <form action="{{ route('admin.files.update', $file) }}" method="POST" enctype="multipart/form-data">
+            <form action="{{ route('admin.files.update', $file) }}" method="POST" enctype="multipart/form-data"
+                class="dropzone border border-gray-300 shadow-md rounded-md p-6" id="myDropzone">
                 @csrf
                 @method('PATCH')
-                <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="file">Nueva Imagen</label>
-                    <input type="file" name="file" id="file" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-                    @error('file')
-                        <span class="text-red-500 text-sm">{{ $message }}</span>
-                    @enderror
-                </div>
-                <div class="flex items-center justify-between">
-                    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Actualizar</button>
-                </div>
             </form>
         </div>
     </div>
+@endsection
+@section('js')
+    <script src="https://cdn.jsdelivr.net/npm/dropzone@5/dist/min/dropzone.min.js"></script>
+    <script>
+        Dropzone.options.myDropzone = {
+            headers: {
+                'X-CSRF-TOKEN': "{{ csrf_token() }}"
+            },
+            dictDefaultMessage: "Actualizar Imagen",
+            acceptedFiles: "image/*",
+            maxFilesize: 5,
+            maxFiles: 1
+        };
+    </script>
 @endsection
